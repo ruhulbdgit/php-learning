b895c8862fb575621dc6947dcbbbaa0b2b1332fb
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2024-12-04 22:41:10 +0600

    1st test
    basic test of php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Learning</title>
+</head>
+
+<body>
+    <h1> Hello world!</h1>
+
+    <?php
+    //ist test with php
+    echo 'Hello World!'; ?>
+
+
+</body>
+
+</html>
