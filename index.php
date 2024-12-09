bed95edc2e523d8af5dcfcd9f0edf50e678e4666
ruhulbdgit <91396640+ruhulbdgit@users.noreply.github.com>
2024-12-09 15:31:51 +0600

    test php
    array

--- index.php
+++ index.php
@@ -39,9 +39,61 @@
     //when i concatenate with ths space betwen two word
     echo "<br>";
     echo $a . " " . $b;
+
+    echo '<hr>';
+    //testing again...
+    //Data types
+    $a = 'Ruhul';
+    echo $a . "<br>"; // this way we can add br tag for next line
+    $b = 6;
+    echo $b . "<br>";
+    $c = 1.5;
+    echo $c . "<br>";
+    $d = true;
+    echo $d . '<br>';
+
+    //array test to show all data
+    $cars = array("Volvo", "BMW", "Toyota");
+    foreach ($cars as $car) {
+        echo $car . "<br>";
+    }
+
+    //index array
+    $indexa = ["One", "Two", "Three"];
+    echo $indexa[0] . '<br>';
+
+    //To Change the Velue in array
+    $changeArry = ["Lalmonirhat", "Dhaka", "Rangpur"];
+    $changeArry[1] = "Kurigram";
+    echo $changeArry[1] . '<hr>';
+
+    //testing To show all data in array
+    $tests = ["Fifty", "Twenty", "Ten"];
+    foreach ($tests as $test) {
+        echo $test . '<br>';
+    }
+    //index array
+    $ttt = ["A", "B", "C", "D"];
+    echo $ttt[3] . '<br>';
+
+    //To show all Item in Array
+    foreach ($ttt as $tt) {
+        echo $tt;
+    }
+
+
+
+    $associtveArry = ["Name" => "Tomas", "Age" => "40", "NickName" => "TTTT"]; {
+        //To Show a fix data like only show name 
+        echo "Tomas is " . $associtveArry['Age'] . " years old.";
+    }
+    //Assosiative array To show All data
+    $associtveArry_test = ["Name" => "Tomas", "Age" => "40", "NickName" => "TTTT"];
+    foreach ($associtveArry_test as $hh => $yy) {
+        echo "$hh : $yy" . '<br>';
+    }
+
     ?>
-
-
 </body>
 
 </html>
